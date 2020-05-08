ebf4c9cbfdad5efb5df8b45a917ad559441aa8f4
rsp9u <gctwt852@yahoo.co.jp>
2020-05-08 13:22:45 +0900

    update

--- src/client/js/util/PreProcessor/SchedausPreProcessor.js
+++ src/client/js/util/PreProcessor/SchedausPreProcessor.js
@@ -1,3 +1,5 @@
+import { Base64 } from 'js-base64';
+
 export class SchedausPreProcessor {
   process(markdown) {
     const regexp = /(^@startgantt(.|[\r\n])*?@endgantt$)/gm;
@@ -7,7 +9,7 @@ export class SchedausPreProcessor {
   replaceToSchedaus(match) {
     const content = match.replace(/^@(start|end)gantt.*$/gm, '');
     const server = match.match(/^@startgantt:(.*)$/m)[1];
-    const b64content = btoa(content);
+    const b64content = Base64.encodeURI(content);
     const url = `http://${server}/sch/svg/${b64content}`;
     return `![gantt-chart](${url})`;
   }
