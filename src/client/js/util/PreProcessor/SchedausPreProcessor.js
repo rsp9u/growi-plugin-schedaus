2c9b7839b8e6af8d28cd60488c6751fdfce58ced
rsp9u <gctwt852@yahoo.co.jp>
2020-05-09 18:17:19 +0900

    update

--- src/client/js/util/PreProcessor/SchedausPreProcessor.js
+++ src/client/js/util/PreProcessor/SchedausPreProcessor.js
@@ -2,22 +2,27 @@ import Cookies from 'js-cookie';
 import { Base64 } from 'js-base64';
 
 export class SchedausPreProcessor {
+  constructor() {
+    this.random_id = Math.random().toString(32).substring(2);
+  }
+
   process(markdown) {
     //console.log('processing schedaus gantt chart..');
     const regexp = /(^@startgantt(.|[\r\n])*?@endgantt$)/gm;
-    return markdown.replace(regexp, this.replaceToSchedaus);
-  }
-
-  replaceToSchedaus(match) {
-    const content = match.replace(/^@(start|end)gantt.*$/gm, '');
-    const b64content = Base64.encodeURI(content);
-    const server = Cookies.get('schedaus_uri');
-    if (server === undefined) {
-      return "Please set 'SCHEDAUS_URI'";
-    } else {
-      const url = `${server}/sch/svg/${b64content}`;
-      return `![gantt-chart](${url})`;
-    }
+    var count = 0;
+    return markdown.replace(regexp, (match) => {
+      const content = match.replace(/^@(start|end)gantt.*$/gm, '');
+      const b64content = Base64.encodeURI(content);
+      const server = Cookies.get('schedaus_uri');
+      if (server === undefined) {
+        return "Please set 'SCHEDAUS_URI'";
+      } else {
+        const client_id = `${this.random_id}+${count}`;
+        const url = `${server}/sch/svg/${b64content}?client_id=${client_id}`;
+        count += 1;
+        return `![gantt-chart](${url})`;
+      }
+    });
   }
 
 }
