38e130ea5da9b15bf920a7319adbde2909839a3c
rsp9u <gctwt852@yahoo.co.jp>
2021-01-30 16:05:16 +0900

    Support @start/endgantt-yaml

--- src/client/js/util/PreProcessor/SchedausYamlPreProcessor.js
+++ src/client/js/util/PreProcessor/SchedausYamlPreProcessor.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie';
 import { Base64 } from 'js-base64';
 
-export default class SchedausPreProcessor {
+export default class SchedausYamlPreProcessor {
 
   constructor() {
     this.random_id = Math.random().toString(32).substring(2);
@@ -10,17 +10,17 @@ export default class SchedausPreProcessor {
   process(markdown) {
     let count = 0;
 
-    // console.log('processing schedaus gantt chart..');
-    const regexp = /(^@startgantt(.|[\r\n])*?@endgantt$)/gm;
+    // console.log('processing yaml gantt chart..');
+    const regexp = /(^@startgantt-yaml(.|[\r\n])*?@endgantt-yaml$)/gm;
     return markdown.replace(regexp, (match) => {
       const server = Cookies.get('schedaus_uri');
       if (server === undefined) {
         return "Please set 'SCHEDAUS_URI' environment variable to the Growi app server";
       }
-      const content = match.replace(/^@(start|end)gantt.*$/gm, '');
+      const content = match.replace(/^@(start|end)gantt-yaml.*$/gm, '');
       const b64content = Base64.encodeURI(content);
       const clientId = `${this.random_id}+${count}`;
-      const url = `${server}/sch/svg/${b64content}?client_id=${clientId}`;
+      const url = `${server}/yaml/svg/${b64content}?client_id=${clientId}`;
       count += 1;
       return `![gantt-chart](${url})`;
     });
