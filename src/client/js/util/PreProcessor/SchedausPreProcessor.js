22b7e739b7303ec7bc3697ef678ba47571aa4d08
rsp9u <gctwt852@yahoo.co.jp>
2021-01-30 18:43:37 +0900

    Fix a bug

--- src/client/js/util/PreProcessor/SchedausPreProcessor.js
+++ src/client/js/util/PreProcessor/SchedausPreProcessor.js
@@ -24,21 +24,6 @@ export default class SchedausPreProcessor {
       count += 1;
       return `![gantt-chart](${url})`;
     });
-
-    // console.log('processing yaml gantt chart..');
-    const regexp = /(^@startgantt-yaml(.|[\r\n])*?@endgantt-yaml$)/gm;
-    return markdown.replace(regexp, (match) => {
-      const server = Cookies.get('schedaus_uri');
-      if (server === undefined) {
-        return "Please set 'SCHEDAUS_URI' environment variable to the Growi app server";
-      }
-      const content = match.replace(/^@(start|end)gantt-yaml.*$/gm, '');
-      const b64content = Base64.encodeURI(content);
-      const clientId = `${this.random_id}+${count}`;
-      const url = `${server}/yaml/svg/${b64content}?client_id=${clientId}`;
-      count += 1;
-      return `![gantt-chart](${url})`;
-    });
   }
 
 }
